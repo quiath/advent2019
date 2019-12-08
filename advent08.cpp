e657b99476e9f4ba5201c47acee282ebb51f484f
Tomasz Kwiatkowski <quiath@gmail.com>
2019-12-08 10:54:01 +0100

    Day 8 part 1

---
+++ advent08.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <ios>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <array>
+
+
+std::pair<int, int> get_layer_counts(std::string_view sv)
+{
+    std::array<int, 256> count{};
+    for (auto ch : sv)
+    {
+        ++count[ch]; 
+    }
+    return { count['0'], count['1'] * count['2'] };
+}
+
+
+int main(int, char**)
+{
+    using namespace std;
+    ifstream inf("input08.txt", ios::in);
+
+    constexpr int sx { 25 } , sy { 6};
+    constexpr int lay_len { sx * sy };
+
+    string input;
+    inf >> input;
+
+    auto len = input.size();
+
+    cout << len << endl;
+
+    pair best_result { lay_len+ 1, 0 };
+
+    for (auto i = 0U; i < len; i += lay_len)
+    {
+        string_view layer(&input[i], sx * sy);
+        auto curr = get_layer_counts(layer);
+        if (curr < best_result) 
+        {
+            best_result = curr;
+        }
+    }
+    cout << best_result.first << "," << best_result.second << endl;
+}
+
