5a6f59d68a4edba0f284b9ecadb1aac283a737ed
Tomasz Kwiatkowski <quiath@gmail.com>
2019-12-19 02:32:45 +0100

    Day 18 part 2 complete (C++)

--- advent18.cpp
+++ advent18.cpp
@@ -14,6 +14,7 @@ using namespace std;
 using VS = vector<string>;
 using VPI = vector<pair<int, int>>;
 using MCPI = map<char, pair<int, int>>;
+using UI = unsigned int;
 
 VS read(const string& s)
 {
@@ -40,6 +41,29 @@ MCPI preprocess(const VS& a)
     }
     return c2p;
 }
+
+pair<MCPI, vector<string>> preprocess_part2(const VS& a)
+{
+    MCPI c2p;
+    VS v(4, "");
+    for (int y = 0; y < a.size(); ++y) {
+        for (int x = 0; x < a[y].size(); ++x) {
+            int part { int(x > a[y].size() / 2) + 2 * (y >= a.size() / 2) };
+            auto ch { a[y][x] };
+            if (ch == '@') {
+                ch = "0123"[part];
+                c2p[ch] = pair{x, y};
+            } else if ('A' <= ch && ch <= 'Z' || 'a' <= ch && ch <= 'z') {
+                c2p[ch] = pair{x, y};
+                if ('a' <= ch && ch <= 'z') {
+                    v[part] += ch;
+                }
+            }
+        }
+    }
+    return { c2p, v };
+}
+
 
 struct Node {
     int x{0};
@@ -72,8 +96,6 @@ pair<int, string> steps_with_keys(const VS& a, int x0, int y0, int x1, int y1, s
     while (q.size()) {
         auto [x, y] = q.front();
         q.pop_front();
-
-        if (DEBUG) { cout << "x:" << x << " y:" << y << endl; }
 
         if (x == x1 && y == y1) {
             // walk back to start using from and return
@@ -135,16 +157,16 @@ pair<int, string> steps_with_keys(const VS& a, int x0, int y0, int x1, int y1, s
     return {-1, keys}; 
 }
 
-unsigned int s2bits(const string& s)
+UI s2bits(const string& s)
 {
-    unsigned int code{0};
+    UI code{0U};
     for (const auto ch: s) {
         code |= 1 << (ch - 'a');
     }
     return code;
 }
 
-int main(int, char**)
+void part1()
 {
     VS a = read("input18.txt");
     //VS a = read("input18c.txt");
@@ -180,7 +202,7 @@ int main(int, char**)
     multimap<int, pair<string, string>> pq;
 
     // path, keys
-    set<pair<string, string>> impossible;
+    //set<pair<string, string>> impossible;
 
     map<pair<char, unsigned int>, int> best_at_pos_with_keys;
 
@@ -195,22 +217,18 @@ int main(int, char**)
 
         pq.erase(pq.begin());
 
-        cout << path << "," << keys << "," << cost << endl;
+        if (DEBUG) { cout << path << "," << keys << "," << cost << endl; }
 
         if (keys.size() == key_count) {
             min_steps = min(cost, min_steps);
             break;
         }
 
-        //int cost = 0;
-        //string path = "@";
-        //string keys = "";
-        
         for (auto it = c2p.find('a'); it != c2p.end(); ++it) {
-            auto prevch = path.back();
-            auto [ prevx, prevy ] = c2p[prevch];
             auto [ nch, np ] = *it;
             if (keys.find(nch) == string::npos) {
+                auto prevch = path.back();
+                auto [ prevx, prevy ] = c2p[prevch];
                 auto [ nx, ny ] = np;
                 auto [ nsteps, nkeys ] = steps_with_keys(a, prevx, prevy, nx, ny, keys);
                 if (nsteps >= 0) {
@@ -227,6 +245,91 @@ int main(int, char**)
     
     cout << "Min count " << min_steps << endl;
 
-    return 0;
+
+}
+
+void part2()
+{
+    VS a = read("input18_2.txt");
+
+    auto [ c2p, part_keys] = preprocess_part2(a);
+
+    for (const auto& s : a) {
+        cout << s << '\n';
+    }
+
+    for (const auto& pr : c2p) {
+        cout << pr.first << ":" << pr.second.first << "," << pr.second.second << endl;
+    }
+
+    for (auto s : part_keys) {
+        cout << s << endl;
+    }
+
+    auto key_count = distance(c2p.find('a'), c2p.end());
+
+    cout << "key count " << key_count << endl;
+
+    // cost to positions [4],  keys. paths are not used!
+    multimap<int, pair<string, string>> pq;
+
+    map<pair<string, UI>, int> best_at_pos_with_keys; // for 4 pos
+
+    pq.insert(pair{0, pair{"0123", ""}});
+
+    int min_steps = 10'000'000;
+
+    while (!pq.empty()) {
+        
+        auto [ cost, pr ] = *pq.begin();
+        auto [ last_pos, keys ] = pr;
+
+        pq.erase(pq.begin());
+
+        if (DEBUG) { cout << last_pos << "," << keys << "," << cost << endl; }
+
+        if (keys.size() == key_count) {
+            min_steps = min(cost, min_steps);
+            break;
+        }
+
+        
+        for (auto k = 0; k < 4; ++k) {
+            for (auto key : part_keys[k]) {
+                if (keys.find(key) == string::npos) {
+                    auto prevch = last_pos[k];
+                    auto [ prevx, prevy ] = c2p[prevch];
+                    auto [ nx, ny ] = c2p[key];
+                    auto [ nsteps, nkeys ] = steps_with_keys(a, prevx, prevy, nx, ny, keys);
+                    if (nsteps >= 0) {
+                        string new_pos(last_pos);
+                        new_pos[k] = key;
+                        auto idx = pair{new_pos, s2bits(nkeys) };
+                        auto prev_best_cost = best_at_pos_with_keys[idx];
+                        if (prev_best_cost == 0 || cost + nsteps < prev_best_cost) { 
+                            pq.insert(pair{cost + nsteps, pair{new_pos, nkeys}});
+                            best_at_pos_with_keys[idx] = cost + nsteps;
+                        }
+
+
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+    cout << "Min count " << min_steps << endl;
+
+}
+
+int main(int, char**)
+{
+    part1();
+    part2();
+    return 0; 
+
 }
 
